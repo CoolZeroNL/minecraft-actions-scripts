05891597e5d8963c882ec4adc3cd3e9ab98fbadb
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2020-01-04 15:29:16 +0100

    Update backup_world.sh

--- backup_world.sh
+++ backup_world.sh
@@ -5,5 +5,16 @@ folder=${2:-''}
 NOW=$(date +"%m-%d-%Y-%H-%M-%S")
 FILE="backup-world.$NOW.$hostname.tar.gz"
 
-sudo tar -czf /home/minecraft/backuped/$FILE /home/minecraft/deployed/$folder/world
-echo "done"
+sudo mkdir -p /home/minecraft/backuped/$hostname
+
+sudo tar -czf /home/minecraft/backuped/$hostname/$FILE /home/minecraft/deployed/$folder/world
+if [[ $? -ne 0 ]]
+then
+    echo "Something went wrong"
+    exit 1
+else
+    echo "Job complete"
+    
+    # Remove all files more then 10 copies.
+    rm -f $(ls -1t /home/minecraft/backuped/ | tail -n +11)
+fi
