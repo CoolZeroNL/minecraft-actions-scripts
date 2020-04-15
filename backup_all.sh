e4e56394ff4de05588dc2b7e7654a6ced723f202
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2020-04-15 11:36:32 +0200

    Update backup_all.sh

--- backup_all.sh
+++ backup_all.sh
@@ -18,5 +18,5 @@ else
     
     # Remove all files more then 10 copies.
     # sudo rm -f $(sudo ls -1t /home/minecraft/backuped/$hostname | grep backup | grep -v world | grep $hostname | tail -n +11)
-    # sudo find /home/minecraft/backuped/$hostname -mindepth 1 -maxdepth 1 -printf '%T@\t%p\n' | sort -n | grep -v 'backup-world' | tail -n +11 | cut -f2- | tr '\n' '\0' | sudo xargs -0 rm -r --
+    sudo find /home/minecraft/backuped/$hostname -mindepth 1 -maxdepth 1 -printf '%T@\t%p\n' | sort -n | grep -v 'backup-world' | head -n +1 | cut -f2- | tr '\n' '\0' | sudo xargs -0 rm -r --
 fi
